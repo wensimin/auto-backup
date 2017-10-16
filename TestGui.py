4663f77ceb2c73f6085f10b580601bbdf605ddf1
wensimin <hukaizhiyu2@gmail.com>
2017-10-16 22:49:37 +0800

    测试gui特性

--- TestGui.py
+++ TestGui.py
@@ -1,18 +1,21 @@
 import threading
 import tkinter as tk
 
+import time
+
 
 class Job(threading.Thread):
     def __init__(self, console):
         super().__init__()
         self.console = console
         self.running = True
+        self.count = 0
 
     def run(self):
-        count = 0
         while self.running:
-            count = count + 1
-            self.console.set(count)
+            self.count = self.count + 1
+            self.console.set(self.count)
+            time.sleep(1)
 
     def stop(self):
         self.running = False
@@ -26,16 +29,21 @@ class Application(tk.Frame):
         self.hi_there = tk.Button(self)
         self.quit = tk.Button(self, text="QUIT", fg="red")
         self.job = Job(self.console)
-        self.quit["command"] = lambda: self.job.stop() & self.job.join() & exit(0)
-        self.console = tk.Label(self, textvariable=self.console)
+        self.quit["command"] = lambda: exit()
+        self.consoleLabel = tk.Label(self, textvariable=self.console)
         self.create_widgets()
+
+    def exit(self):
+        if self.job.is_alive():
+            self.job.stop() & self.job.join()
+        exit(0)
 
     def create_widgets(self):
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="left")
         self.quit.pack(side="right")
-        self.console.pack(side="bottom")
+        self.consoleLabel.pack(side="bottom")
 
     def say_hi(self):
         if self.job.is_alive():
