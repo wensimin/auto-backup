12b4b324d0153072d5c3047b28f3d2761719d768
wensimin <hukaizhiyu2@gmail.com>
2017-10-21 07:01:48 +0800

    gui完成

--- FtpUtil.py
+++ FtpUtil.py
@@ -1,23 +1,50 @@
+import ftplib
 import os
 from ftplib import error_perm
 
 
-def placeFiles(ftp, path, bash_path="/"):
+def placeFiles(ftp, path, bash_path="/", log=lambda m: print(m)):
+    if not bash_path.endswith("/"):
+        bash_path += "/"
     for name in os.listdir(path):
         localpath = os.path.join(path, name)
         if os.path.isfile(localpath):
-            print("上传文件", bash_path + name, localpath)
+            log("上传文件" + bash_path + name + localpath)
             ftp.storbinary('STOR ' + bash_path + name, open(localpath, 'rb'))
         elif os.path.isdir(localpath):
-            print("创建目录", bash_path + name)
+            log("创建目录" + bash_path + name)
             try:
                 ftp.mkd(bash_path + name)
             # ignore "directory already exists"
             except error_perm as e:
                 if not e.args[0].startswith('550'):
                     raise
-            print("移动到", bash_path + name)
             ftp.cwd(bash_path + name)
-            placeFiles(ftp, localpath, bash_path + name + "/")
-            print("移动到", "..")
+            placeFiles(ftp, localpath, bash_path + name, log)
             ftp.cwd("..")
+
+
+def downFiles(ftp, path, bash_path="/", log=lambda m: print(m)):
+    if not path.endswith("/"):
+        path += "/"
+    for file in ftp.nlst(bash_path):
+        remote_path = file
+        name = os.path.basename(file)
+        if is_ftp_dir(ftp, remote_path):
+            new_path = os.path.join(path, name)
+            os.mkdir(new_path)
+            log("创建目录 " + new_path)
+            downFiles(ftp, new_path, remote_path, log)
+        else:
+            local_path = os.path.join(path, name)
+            with open(local_path, "wb") as f:
+                log("下载文件 " + local_path)
+                ftp.retrbinary('RETR %s' % remote_path, f.write)
+
+
+def is_ftp_dir(ftp, path):
+    try:
+        ftp.cwd(path)
+        return True
+    except ftplib.error_perm:
+        return False
